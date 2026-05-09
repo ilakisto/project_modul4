d27cfa90703d2c90f5eb3a11912a07e2daa59d3a
Анюта <anafle@yandex.ru>
2026-05-09 17:59:32 +0300

    commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -31,4 +31,32 @@ img1.addEventListener('mouseleave', function(){
   img1.querySelectorAll('.grid-cell').forEach(c => c.classList.remove('active'));
 });
 
+
+
+
+
+
+
+let carts = document.querySelectorAll(".carts");
+carts.forEach(cart =>{
+    cart.addEventListener('mouseenter', function(){
+        carts.forEach(c =>{
+            c.style.width = '18.13vw';
+            c.style.height = '22.08vw';
+        });
+        cart.style.width = '28.06vw';
+        cart.style.height = '35.14vw';
+    });
+    cart.addEventListener('mouseleave', function(){
+        carts.forEach(c =>{
+            c.style.width = '18.13vw';
+            c.style.height = '22.08vw';
+        });
+        carts[0].style.width = '28.06vw';
+        carts[0].style.height = '35.14vw';
+    });
+});
+
+
+
 });
