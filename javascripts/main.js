0aa425ebea4303e22528eed31e68f081295d0698
Анюта <anafle@yandex.ru>
2026-05-10 17:34:45 +0300

    commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 let img1 = document.querySelector('.img1');
 let COLS = 10, ROWS = 6;
-let SRC = './img/img1.jpg';
+let SRC = './img/img1.png';
 
 for (let r = 0; r < ROWS; r++) {
   for (let c = 0; c < COLS; c++) {
@@ -30,11 +30,6 @@ img1.addEventListener('mousemove', function(e) {
 img1.addEventListener('mouseleave', function(){
   img1.querySelectorAll('.grid-cell').forEach(c => c.classList.remove('active'));
 });
-
-
-
-
-
 
 
 let carts = document.querySelectorAll(".carts");
