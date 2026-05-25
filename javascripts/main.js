0911b9b745feff33c8435f464d8c553e31eae982
Анюта <anafle@yandex.ru>
2026-05-25 20:15:36 +0300

    commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -32,6 +32,13 @@ document.addEventListener("DOMContentLoaded", function() {
 // });
 
 
+ // use a script tag or an external JS file
+ document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ExpoScaleEase)
+  // gsap code here!
+ });
+
+
 
 
 
